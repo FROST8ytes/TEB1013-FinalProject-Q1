f565135f967a246eb1f19e3bbd6e12adee3abef6
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2023-03-15 02:45:04 +0800

    Implement getTimeInput to accept time input from the user.

--- common.cpp
+++ common.cpp
@@ -8,6 +8,30 @@
 
 namespace common {
     /**
+     * This function is meant to handle accepting time input in 24-hour format.
+     * It keeps asking for the input if hour is not 0 <= hour < 24,
+     * and if minute is not 0 <= minute < 60,
+     * and if the delimiter is not ':'.
+     * @param message the message that is printed out before waiting for an input
+     * @return a Time struct with valid hour and minute
+     */
+    Time getTimeInput(const char* message) {
+        Time time{};
+        char delimiter;
+        bool validDelimiter, validHour, validMinute;
+
+        do {
+            std::cout << message << " (HH:MM 24-hour format): ";
+            std::cin >> time.hour >> delimiter >> time.minute;
+            std::cin.ignore();
+            validDelimiter = delimiter == ':';
+            validHour = time.hour >= 0 && time.hour < 24;
+            validMinute = time.minute >= 0 && time.minute < 60;
+        } while (!(validDelimiter && validHour && validMinute));
+        return time;
+    }
+
+    /**
      * This function is meant to be used when a string input is needed from the user.
      * The message is printed out first before accepting input.
      * It keeps asking for the input if the user types values outside of the input constraint(s).
@@ -19,7 +43,6 @@ namespace common {
         std::string input;
         bool isValid;
         do {
-            std::cin.ignore();
             std::cout << message << ": ";
             std::getline(std::cin, input);
             std::cin.ignore();
--- common.h
+++ common.h
@@ -9,6 +9,24 @@
 #include <string>
 
 namespace common {
+    /**
+     * Stores hour and minute.
+     * Used to represent time.
+     */
+    struct Time {
+        unsigned int hour;
+        unsigned int minute;
+    };
+
+    /**
+     * This function is meant to handle accepting time input in 24-hour format.
+     * It keeps asking for the input if hour is not 0 <= hour < 24,
+     * and if minute is not 0 <= minute < 60,
+     * and if the delimiter is not ':'.
+     * @param message the message that is printed out before waiting for an input
+     * @return a Time struct with valid hour and minute
+     */
+    Time getTimeInput(const char* message);
 
     /**
      * This function is meant to be used when a non-string input is needed from the user.
