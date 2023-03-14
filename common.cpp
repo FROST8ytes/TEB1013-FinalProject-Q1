791ab835c369a40879ee8836d0f80fa1cfc76889
Ammar Farhan Mohamad Rizam <ammar_22006911@utp.edu.my>
2023-03-15 01:59:32 +0800

    Implement common functions for accepting non-string and string inputs from user.

--- common.cpp
+++ common.cpp
@@ -2,4 +2,29 @@
 // Created by ammar on 15/3/2023.
 //
 
+#include <iostream>
+#include <string>
 #include "common.h"
+
+namespace common {
+    /**
+     * This function is meant to be used when a string input is needed from the user.
+     * The message is printed out first before accepting input.
+     * It keeps asking for the input if the user types values outside of the input constraint(s).
+     * @param message the message that is printed out before waiting for an input
+     * @param func a function that accepts the input as its parameter and returns a bool, to check for input constraints.
+     * @return returns a valid--well within constraint(s)--string.
+     */
+    std::string getStrInput(const char* message, bool (*func)(std::string)) {
+        std::string input;
+        bool isValid;
+        do {
+            std::cin.ignore();
+            std::cout << message << ": ";
+            std::getline(std::cin, input);
+            std::cin.ignore();
+            isValid = func(input);
+        } while (!isValid);
+        return input;
+    }
+}
--- common.h
+++ common.h
@@ -5,4 +5,43 @@
 #ifndef TEB1013_FINALPROJECT_Q1_COMMON_H
 #define TEB1013_FINALPROJECT_Q1_COMMON_H
 
+#include <iostream>
+#include <string>
+
+namespace common {
+
+    /**
+     * This function is meant to be used when a non-string input is needed from the user.
+     * The message is printed out first before accepting input.
+     * It keeps asking for the input if the user types a value outside of the input constraint(s).
+     * @tparam T data-type of the input
+     * @param message the message that is printed out before waiting for an input
+     * @param func a function that accepts the input as its parameter and returns a bool, to check for input constraints.
+     * @return returns valid input
+     * @note The function definition is needed in the header file to avoid suffering from missing a valid instantiation.
+     * @note The above only applies to templates.
+     */
+    template<typename T>
+    inline T getNumInput(const char* message, bool (*func)(T)) {
+        T input;
+        bool isValid;
+        do {
+            std::cout << message << ": ";
+            std::cin >> input;
+            isValid = func(input);
+        } while (!isValid);
+        return input;
+    }
+
+    /**
+     * This function is meant to be used when a string input is needed from the user.
+     * The message is printed out first before accepting input.
+     * It keeps asking for the input if the user types values outside of the input constraint(s).
+     * @param message the message that is printed out before waiting for an input
+     * @param func a function that accepts the input as its parameter and returns a bool, to check for input constraints.
+     * @return returns a valid--well within constraint(s)--string.
+     */
+    std::string getStrInput(const char* message, bool (*func)(std::string));
+}
+
 #endif //TEB1013_FINALPROJECT_Q1_COMMON_H
